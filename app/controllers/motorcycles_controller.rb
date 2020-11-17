d641c264dc580af7d6748938f36acb2493fbe504
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-17 16:56:08 -0500

    fixed issue with motorcycle controller missin an end

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -6,11 +6,11 @@ class MotorcyclesController < ApplicationController
   end
 
   def edit
-      @motorcycle = Motorcycle.find(params[:id])
-    end
+    @motorcycle = Motorcycle.find(params[:id])
+  end
 
   def new
-      @motorcycle = Motorcycle.new
+    @motorcycle = Motorcycle.new
   end
 
   def show
@@ -19,13 +19,13 @@ class MotorcyclesController < ApplicationController
   end
 
   def create
-      @motorcycle = Motorcycle.new(motorcycle_params)
-      @motorcycle.user = current_user
-      if @motorcycle.save
-        redirect_to motorcycle_path(@motorcycle)
-      else
-        render :new
-      end
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    @motorcycle.user = current_user
+    if @motorcycle.save
+      redirect_to motorcycle_path(@motorcycle)
+    else
+      render :new
+    end
   end
 
   def update
@@ -35,20 +35,21 @@ class MotorcyclesController < ApplicationController
   end
 
   def destroy
-  motorcycle = Motorcycle.find(params[:id])
-  if current_user == motorcycle.user
-    motorcycle.destroy
-    redirect_to show_motorcycle_user_path(current_user)
-  else
-    render(
-      html: "<script>alert('You can't delete this motorcycle post because you are not the owner')</script>".html_safe,
-      layout: 'application'
-    )
+    motorcycle = Motorcycle.find(params[:id])
+      if current_user == motorcycle.user
+        motorcycle.destroy
+        redirect_to show_motorcycle_user_path(current_user)
+      else
+        render(
+          html: "<script>alert('You can't delete this motorcycle post because you are not the owner')</script>".html_safe,
+          layout: 'application'
+        )
+      end
   end
 
   private
 
   def motorcycle_params
-      params.require(:motorcycle).permit(:model, :year, :location, :brand)
+    params.require(:motorcycle).permit(:model, :year, :location, :brand)
   end
 end
