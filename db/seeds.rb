40a1fc4666cf5f06041f9fbb600b53645cd8f3b4
Patrick McAnuff <patrickmcanuff@icloud.com>
2020-11-16 22:00:09 -0500

    seed progress; photos of motorcycles are stored offline for now but data is complete; need to clarify bookings & user fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying users..."
+User.destroy_all if Rails.env.development?
+
+puts "Destroying bookings..."
+Booking.destroy_all if Rails.env.development?
+
+puts "Destroying motorcycles..."
+Motorcycle.destroy_all if Rails.env.development?
+
+puts "Creating users..."
+30.Times do
+  User.create(
+      name: Faker::Name.name,
+      address: Faker::Address.full_address,
+      drivers_license_number: Faker::DrivingLicence.british_driving_licence,
+  )
+end
+
+puts "Creating bookings..."
+Booking.create()
+
+puts "Creating motorcycles..."
+Motorcycle.create(year: "2018", brand: "Aprilia", model: "Shiver 900", location: "Lisbon, Portugal")
+Motorcycle.create(year: "2019", brand: "Yamaha", model: "YZ250", location: "Canggu, Bali")
+Motorcycle.create(year: "2015", brand: "KTM", model: "1290 Super Duke R", location: "Berlin, Germany")
+Motorcycle.create(year: "2014", brand: "BMW", model: "F700GS", location: "Porto, Portugal")
+Motorcycle.create(year: "2011", brand: "Harley-Davidson", model: "Electra Glide", location: "Playa Del Carmen, Mexico")
+Motorcycle.create(year: "2017", brand: "BMW", model: "S1000R", location: "Calgary, Canada")
+Motorcycle.create(year: "2003", brand: "Yamaha", model: "YZF1000R", location: "Austin, Texas, United States")
+Motorcycle.create(year: "2019", brand: "Honda", model: "CB1000R", location: "Sydney, Australia")
+Motorcycle.create(year: "2012", brand: "Triumph", model: "Scrambler", location: "Vancouver, Canada")
+Motorcycle.create(year: "2014", brand: "Moto Guzzi", model: "V7 II Stone", location: "Budapest, Hungary")
+Motorcycle.create(year: "2019", brand: "Husqvarna", model: "Vitpilen 701", location: "Tenerife, Canary Islands, Spain")
+Motorcycle.create(year: "2017", brand: "KTM", model: "250 EXC-F", location: "Chiang Mai, Thailand")
