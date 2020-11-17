e1b0df671d9fcabe453fd6750b3f5bf61b416cbf
Camilla Bazakov <bazakovc@gmail.com>
2020-11-17 13:19:45 -0500

    added show action and coded view

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,2 +1,7 @@
 class MotorcyclesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @motorcycle = Motorcycle.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_205605) do
+ActiveRecord::Schema.define(version: 2020_11_17_034953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 2020_11_16_205605) do
   create_table "motorcycles", force: :cascade do |t|
     t.string "model"
     t.string "brand"
-    t.string "type"
     t.string "location"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "year"
     t.index ["user_id"], name: "index_motorcycles_on_user_id"
   end
 
