45c056f84c64f6dc09383450c891ecd90162546b
Camilla Bazakov <bazakovc@gmail.com>
2020-11-17 14:08:11 -0500

    added link to bookings/new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'motorcycles#home'
 
   resources :motorcycles
+  resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/users/:id', to: 'users#show', as: :user_show
