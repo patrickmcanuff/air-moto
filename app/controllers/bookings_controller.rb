f397b0c6b05bbbd4a1a6c5fc481f76910d036bfa
Camilla Bazakov <bazakovc@gmail.com>
2020-11-18 12:33:05 -0500

    deleted new method in bookings, coded create method, changed routes for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,21 +2,22 @@ class BookingsController < ApplicationController
 
   def index
     #add the logic if user is the current user then display all the bookings
+    #if the motorcycle is booked by someone else I shouldn't be able to book it for same dates
     # @booking = Booking.find()
-    # if booking.user == current_user
-      @bookings = Booking.all
+     @bookings = current_user.bookings
     # end
   end
 
-  def new
-    @booking = Booking.new
-    @motorcycle = Motorcycle.find(params[:motorcycle_id])
-  end
-
   def create
-    booking = Booking.new(booking_params)
-    booking.save
-    redirect_to bookings_path(booking)
+    @motorcycle = Motorcycle.find(params[:motorcycle_id])
+    @booking = Booking.new(booking_params)
+    @booking.motorcycle = @motorcycle
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'motorcycles/show'
+    end
   end
 
   def booking_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :motorcycles
-  resources :bookings
-  
+  resources :motorcycles do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index,:destroy]
+
   root to: 'motorcycles#home'
   get '/users/:id', to: 'users#show', as: :user_show
   get '/users/:id/show_motorcycle', to: 'users#show_motorcycle', as: :show_motorcycle_user
+  get '/users/:id/bookings', to: 'users#index', as: :your_bookings
 end
