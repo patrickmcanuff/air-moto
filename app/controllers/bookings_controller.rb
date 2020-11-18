62572f30206c9e56f329bcf5e7cd4c12c91297dd
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-18 11:42:53 -0500

    added destroy method to the booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-
   def new
     @booking = Booking.new
     @motorcycle = Motorcycle.find(params[:motorcycle_id])
@@ -13,5 +12,18 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:date_start, :date_end)
+  end
+
+  def destroy
+    booking = Booking.find(params[:id])
+    if current_user == booking.user
+      booking.destroy
+      redirect_to bookings_path
+    else
+      render(
+        html: "<script>alert('You can't delete this booking because you are not the owner of the booking')</script>".html_safe,
+        layout: 'application'
+      )
+    end
   end
 end
