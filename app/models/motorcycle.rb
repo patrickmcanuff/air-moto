cef5438bc980e4d2f7de928494b6d67750eb55a3
sarah dakkak <sarah.dakkak1@gmail.com>
2020-11-18 13:52:33 -0500

    fixed home page

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,9 +1,8 @@
 class Motorcycle < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  # validates :model, presence: true
-  # validates :brand, presence: true
-  # validates :location, presence: true
-  # validates :price, presence: true
-  # validates :year, presence: true
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :location, presence: true
+  validates :year, presence: true
 end
