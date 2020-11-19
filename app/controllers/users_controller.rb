64c1ce1d08f77d2768d9c86119a574a924347797
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-19 11:56:24 -0500

    added booked column + first pass at the display of the booking in the profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-
   def show
+    @booking = Booking.where(user: current_user.id)
   end
 
   def show_motorcycle
     @motorcycles_user = Motorcycle.where(user: current_user.id)
+  end
+
+  def booked_decision
+    if params[:decision] == 'accepted'
+      booking_to_change = Booking.where(params[:booking_id])
+      booking_to_change.booked = 'accepted'
+    elsif params[:decision] == 'refused'
+      booking_to_change = Booking.where(params[:booking_id])
+      booking_to_change.booked = 'refused'
+    end
   end
 end
