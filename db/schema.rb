1e06ad06fd608736f329c49aa74284d0826c073b
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-20 14:43:14 -0500

    fixed bug with the login

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_19_213425) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_213425) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "year"
     t.integer "price"
+    t.string "photo"
     t.index ["user_id"], name: "index_motorcycles_on_user_id"
   end
 
